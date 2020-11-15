374a5466ae4234725a07ed59438ad833de2aa21a
treguv <vladislavtregubov00@gmail.com>
2020-11-14 17:31:43 -0800

    toc changes

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -5,35 +5,45 @@ function generateMarkdown(data) {
   ${data.projectDescription}
   
   ## Table Of Contents
-  1. [Installation])(#installation)
-  2. [Usage])(#Usage)
-  3. [License])(#License)
-  4. [Contributing])(#Contributing)
-  5. [Tests])(#Tests)
-  6. [Questions])(#Questions)
-  7. [Credits])(#Credits)
+  1. [Installation](#installation)
+  2. [Usage](#Usage)
+  3. [License](#License)
+  4. [Contributing](#Contributing)
+  5. [Tests](#Tests)
+  6. [Questions](#Questions)
+  7. [Credits](#Credits)
 
   ## Installation
   <a name = "installation">${data.projectInstallInstructions} </a>
 
   ## Usage
-  ${data.projectUsage}
+  <a name = "Usage">${data.projectUsage}</a>
   
   ## License
+  <a name = "License">
   This application is covered under ${data.projectLicense}
+  </a>
   
   ## Contributing
+  <a name = "Contributing">
   ${data.projectContribution}
+  </a>
 
   ## Tests 
+  <a name = "Tests">
   ${data.projectTestInstructions}
+  </a>
 
   ## Questions
+  <a name = "Questions">
   [Github:](http://github.com/${data.userGithub})
   With additional questions contact me via email: ${data.userEmail}
+  </a>
 
   ## Credits
+  <a name = "Credits">
   ${data.projectContributors}
+  </a>
 `;
 }
 
